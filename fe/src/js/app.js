2a672e0b96fab1e4baa591009f6215f82ce46496
junyeoplee <jyeop920@gmail.com>
2023-04-21 23:16:57 +0900

    추가 버튼 기능 구현
    디자인 조금 수정 하고 추가 버튼 기능 구현

--- fe/src/js/app.js
+++ fe/src/js/app.js
@@ -3,36 +3,50 @@ const userInput = document.querySelector("#input");
 const submitButton = document.querySelector("#submit-button");
 const todoList = document.querySelector(".task-container");
 
+let menu = [];
 
+const render = () => {
+    if(userInput.value !== "") {
+        menu.push({list:userInput.value});
+        console.log(menu);
+    }
+    const template = menu.map(item => {
+        return `
+        <div class="completed-container">
+        <ul id="completed-list">
+            <li>
+                <input class = "checkbox" type="checkbox"/>
+                <span style="color: white; font-size: 18px;">
+                    ${item.list}
+                </span>
+                <div>
+                <button 
+                    class="task-button"  
+                    value="수정">수정
+                </button>
+                <button 
+                    class="task-button" 
+                    value="삭제">삭제
+                </button>
+                </div>
+            </li>
+        </ul>
+    </div>`
+    }).join('');
 
-const addTodoList = () => {
-    const template = 
-    `<div class="completed-container">
-    <ul id="completed-list">
-        <li>
-            <input class = "checkbox" type="checkbox"/>
-            <span style="color: white; font-size: 18px;">
-                ${userInput.value}
-            </span>
-            <div>
-            <button 
-                class="task-button"  
-                value="수정">수정
-            </button>
-            <button 
-                class="task-button" 
-                value="삭제">삭제
-            </button>
-            </div>
-        </li>
-    </ul>
-</div>`
-todoList.innerHTML += template;
-userInput.value = '';
+todoList.innerHTML = template;
 }
 
-submitButton.addEventListener("click" , addTodoList);
-submitButton.addEventListener("submit" , addTodoList);
+const addList = () => {
+    if(userInput.value === '') {
+        return alert("할일을 입력하세요^^");
+    }
+    render();
+    userInput.value = '';
+}
+
+submitButton.addEventListener("click" , addList);
+submitButton.addEventListener("submit" , addList);
 
 
 menuForm.addEventListener("submit", (event) => {
