0e4b607d257b878902639c160c53d6ae030936a7
junyeoplee <jyeop920@gmail.com>
2023-04-21 22:25:09 +0900

    인풋에 입력한 값 리스트에 추가
    리스트에 입력한 값이 추가 되도록 구현

--- fe/src/js/app.js
+++ fe/src/js/app.js
@@ -5,5 +5,37 @@ const todoList = document.querySelector(".task-container");
 
 
 
+const addTodoList = () => {
+    const template = 
+    `<div class="completed-container">
+    <ul id="completed-list">
+        <li>
+            <input class = "checkbox" type="checkbox"/>
+            <span style="color: white; font-size: 18px;">
+                ${userInput.value}
+            </span>
+            <div>
+            <button 
+                class="task-button"  
+                value="수정">수정
+            </button>
+            <button 
+                class="task-button" 
+                value="삭제">삭제
+            </button>
+            </div>
+        </li>
+    </ul>
+</div>`
+todoList.innerHTML += template;
+userInput.value = '';
+}
 
+submitButton.addEventListener("click" , addTodoList);
+submitButton.addEventListener("submit" , addTodoList);
+
+
+menuForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+});
 
